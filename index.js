b14905348284985cffb61d3b5ff47c74358e36c5
mas-alissa <mas.webdesigner@gmail.com>
2023-11-16 11:10:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb+srv://admin-mohammed:Minna2018@cluster0.c4wkhb3.mongodb.net/myDB?retryWrites=true&w=majority")
+const userSchema = new mongoose.Schema({
+    name:String,
+    age:Number,
+})
+const User = mongoose.model("user",userSchema);
+
+
+app.get("/", async(req,res)=>{
+ 
+    const data = await User.find({});
+    res.json(data);
+
+//   
+
+
+
+// newUser.save();
+})
+app.listen(3000,()=>{
+    console.log("Server is running !")
+})
